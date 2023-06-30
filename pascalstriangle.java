e91f0bfea04f750f7e8b47237f821d1f35d83957
JaneshKapoor <99019106+JaneshKapoor@users.noreply.github.com>
2023-07-01 04:28:10 +0530

    Pascals triangle update code

---
+++ pascalstriangle.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.lang.Integer;
+
+public class pascalstriangle {
+
+    public static ArrayList<Integer> generaterow(int n){
+        int res = 1;
+        ArrayList<Integer> templist = new ArrayList<Integer>();
+        templist.add(1);
+        for(int i = 1;i<n;i++){
+            res = res * (n-i);
+            res = res/(i);
+            templist.add(res);
+        }
+        return templist;
+    }
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1;i<=n;i++){
+            list.add(generaterow(i));
+        }
+        System.out.println(list);
+        sc.close();
+    }
+}
