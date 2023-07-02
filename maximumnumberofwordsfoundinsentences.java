0728f4b499965f38e8e29eaa4ade0ceb47350da1
JaneshKapoor <99019106+JaneshKapoor@users.noreply.github.com>
2023-07-03 01:11:57 +0530

    Max no. of words program updated

--- maximumnumberofwordsfoundinsentences.java
+++ maximumnumberofwordsfoundinsentences.java
@@ -1,11 +1,18 @@
 public class maximumnumberofwordsfoundinsentences {
-    public static void main(int[] args) {
+    public static void main(String[] args) {
         int max = 0;
         String sentences[] = {"alice and bob love leetcode", "i think so too", "this is great thanks very much"};
-        for(String i : sentences){
-            String[] w = i.split("\\s+");
-            if(w.length>=max){
-                max = w.length;
+        for(int i = 0;i<sentences.length;i++){
+            int count = 0;
+            int words = 0;
+            for(int j = 0;j<sentences[i].length();j++){
+                if(sentences[i].charAt(j) == ' '){
+                    count = count + 1;
+                }
+                words = count + 1;
+            }
+            if(words>max){
+                max = words;
             }
         }
         System.out.println(max);
