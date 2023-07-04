2b1b224e98310309cb0413d138d6bcf819ff7617
JaneshKapoor <99019106+JaneshKapoor@users.noreply.github.com>
2023-07-05 01:25:32 +0530

    Kadane's Algorithm in JAVA

---
+++ maximumsubarray.java
@@ -0,0 +1,19 @@
+public class maximumsubarray {
+    public static void main(String[] args) {
+        //Kadane's Algorithm
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0;i<nums.length;i++){
+            sum = sum + nums[i];
+
+            if(sum>max){
+                max = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        System.out.println(max);
+    }
+}
