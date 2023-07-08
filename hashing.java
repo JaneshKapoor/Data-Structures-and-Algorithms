d723f95efb8bab9f60e78a34372017b52940f98d
JaneshKapoor <99019106+JaneshKapoor@users.noreply.github.com>
2023-07-08 15:54:35 +0530

    Hashset in Java

---
+++ hashing.java
@@ -0,0 +1,39 @@
+import java.util.HashSet;
+// import java.util.Iterator;
+public class hashing {
+    public static void main(String[] args) {
+        //Creation
+        HashSet<Integer> set = new HashSet<>();
+        //Insertion
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        //Search - contains
+        // if(set.contains(1)){
+        //     System.out.println("Set contains 1");
+        // }
+        // if(!(set.contains(6))){
+        //     System.out.println("Does not contain");
+        // }
+
+        // //Delete
+        // set.remove(1);
+        // if(!(set.contains(1))){
+        //     System.out.println("does not contain 1");
+        // }
+
+        // System.out.println("Size of set is : " + set.size());
+
+        //Print all elements
+        // System.out.println(set);
+
+        //Iterator
+        // Iterator it = set.iterator();
+        // //hasNext; next
+        // while(it.hasNext()){
+        //     System.out.println(it.next());
+        // }
+
+    }
+}
